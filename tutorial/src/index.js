3541a7792c120f716e41c9f5b2c865d75aad0fec
thenatural86 <philip.smalls86@gmail.com>
2020-11-23 14:35:01 -0500

    initial component

---
+++ tutorial/src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const Greeting = () => {
+  return <h4>Hello World</h4>
+}
+
+ReactDOM.render(<Greeting />, document.getElementById("root"))
